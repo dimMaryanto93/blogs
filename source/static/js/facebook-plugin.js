933e95c85c9ff259b58b272cb7891854764ae537
dimMaryanto@Aspire-E14 <software.dimmaryanto@hotmail.com>
2016-07-29 23:00:43 +0700

    tombol ikuti facebook

--- source/static/js/facebook-plugin.js
+++ source/static/js/facebook-plugin.js
@@ -1,15 +1,7 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '858150254289300',
-    xfbml      : true,
-    version    : 'v2.6'
-  });
-};
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.7&appId=1172380132803751";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
