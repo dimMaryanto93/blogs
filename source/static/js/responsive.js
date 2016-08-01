7f4869d06460167e4ce8c0afd1211de893dfa5a4
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2016-08-01 08:19:09 +0700

    update layout post

--- source/static/js/responsive.js
+++ source/static/js/responsive.js
@@ -2,4 +2,19 @@ $(document).ready(function(){
     $(".button-collapse").sideNav();
     $('.materialboxed').materialbox();
     $(".post-content img").addClass("materialboxed responsive-img center-image");
+    var clipboard = new Clipboard('#copyboard');
+
+    clipboard.on('success', function(e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+        $.notify("Telah di salin", "success");
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function(e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+        $.notify("Gagal di salin!", "error");
+    });
 });
